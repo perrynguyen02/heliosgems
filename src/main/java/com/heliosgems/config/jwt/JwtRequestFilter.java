63784ab959131972ce2cabfa6b49cc6e33a9270b
Perry Nguyen <nhlong250702@gmail.com>
2024-02-08 18:20:01 +0700

    jwt authentication

--- src/main/java/com/heliosgems/config/jwt/JwtRequestFilter.java
+++ src/main/java/com/heliosgems/config/jwt/JwtRequestFilter.java
@@ -41,9 +41,10 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        jwt = authHeader.substring(7);
 
+        jwt = authHeader.substring(7);
         username = jwtService.getUsername(jwt);
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
             var isTokenValid = tokenRepo.findByToken(jwt)
